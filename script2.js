491061c2b387c5ed1721131efd799303df21fa9d
ChetanPrince <saini.rajvir2@gmail.com>
2024-08-05 22:30:51 +0200

    two

--- script2.js
+++ script2.js
@@ -11,3 +11,25 @@ function printHighscores() {
                 "highscores"
             )
         ) || [];
+        highscores.sort(function (a, b) {
+            return b.score - a.score;
+        });
+        highscores.forEach(function (
+            score
+        ) {
+            let liTag =
+                document.createElement(
+                    "li"
+                );
+            liTag.textContent =
+                score.name +
+                " - " +
+                score.score;
+            let olEl =
+                document.getElementById(
+                    "highscores"
+                );
+            olEl.appendChild(liTag);
+        });
+    }
+    
