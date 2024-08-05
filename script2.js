6fbe791effbbeec91e27ab3d0e50c5ad36c420e7
ChetanPrince <saini.rajvir2@gmail.com>
2024-08-05 22:29:59 +0200

    one

--- script2.js
+++ script2.js
@@ -1,3 +1,13 @@
 let scoresBtn = document.querySelector(
     "#view-high-scores"
 );
+// Rank previous scores in order by
+// Retrieving scores from localStorage
+
+function printHighscores() {
+    let highscores =
+        JSON.parse(
+            window.localStorage.getItem(
+                "highscores"
+            )
+        ) || [];
